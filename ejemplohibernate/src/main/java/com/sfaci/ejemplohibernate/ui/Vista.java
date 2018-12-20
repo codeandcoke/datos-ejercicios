c22af8c6ed8ba3e347d39df81a7f0b47c0a8b858
Santiago Faci <santiago.faci@gmail.com>
2018-12-20 19:40:34 +0100

    Comenzando con la nueva interfaz de usuario

--- ejemplohibernate/src/main/java/com/sfaci/ejemplohibernate/ui/Vista.java
+++ ejemplohibernate/src/main/java/com/sfaci/ejemplohibernate/ui/Vista.java
@@ -26,6 +26,14 @@ public class Vista extends JFrame {
 
 	public JPanel contentPane;
 	public DefaultListModel<Personaje> mPersonajes;
+	public JLabel lblNewLabel_1;
+	public JTextField textField;
+	public JTextField textField_1;
+	public JTextField textField_2;
+	public JTextField textField_3;
+	public JLabel lblNewLabel_2;
+	public JLabel lblNewLabel_3;
+	public JLabel lblNewLabel_4;
 
 	public Vista() {
 		setTitle("CRUD Hibernate");
@@ -76,12 +84,56 @@ public class Vista extends JFrame {
 		contentPane.add(tabbedPane, BorderLayout.CENTER);
 		
 		JPanel panel = new JPanel();
-		tabbedPane.addTab("New tab", null, panel, null);
+		tabbedPane.addTab("Armas", null, panel, null);
 		
 		JPanel panel_1 = new JPanel();
-		tabbedPane.addTab("New tab", null, panel_1, null);
+		tabbedPane.addTab("Personajes", null, panel_1, null);
 		SpringLayout sl_panel_1 = new SpringLayout();
 		panel_1.setLayout(sl_panel_1);
+		
+		lblNewLabel_1 = new JLabel("New label");
+		sl_panel_1.putConstraint(SpringLayout.NORTH, lblNewLabel_1, 10, SpringLayout.NORTH, panel_1);
+		sl_panel_1.putConstraint(SpringLayout.WEST, lblNewLabel_1, 10, SpringLayout.WEST, panel_1);
+		panel_1.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		sl_panel_1.putConstraint(SpringLayout.NORTH, textField, 0, SpringLayout.NORTH, lblNewLabel_1);
+		sl_panel_1.putConstraint(SpringLayout.WEST, textField, 17, SpringLayout.EAST, lblNewLabel_1);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		sl_panel_1.putConstraint(SpringLayout.NORTH, textField_1, 6, SpringLayout.SOUTH, textField);
+		sl_panel_1.putConstraint(SpringLayout.WEST, textField_1, 0, SpringLayout.WEST, textField);
+		panel_1.add(textField_1);
+		textField_1.setColumns(10);
+		
+		textField_2 = new JTextField();
+		sl_panel_1.putConstraint(SpringLayout.NORTH, textField_2, 6, SpringLayout.SOUTH, textField_1);
+		sl_panel_1.putConstraint(SpringLayout.EAST, textField_2, 0, SpringLayout.EAST, textField);
+		panel_1.add(textField_2);
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		sl_panel_1.putConstraint(SpringLayout.NORTH, textField_3, 6, SpringLayout.SOUTH, textField_2);
+		sl_panel_1.putConstraint(SpringLayout.EAST, textField_3, 0, SpringLayout.EAST, textField);
+		panel_1.add(textField_3);
+		textField_3.setColumns(10);
+		
+		lblNewLabel_2 = new JLabel("New label");
+		sl_panel_1.putConstraint(SpringLayout.NORTH, lblNewLabel_2, 0, SpringLayout.NORTH, textField_1);
+		sl_panel_1.putConstraint(SpringLayout.EAST, lblNewLabel_2, 0, SpringLayout.EAST, lblNewLabel_1);
+		panel_1.add(lblNewLabel_2);
+		
+		lblNewLabel_3 = new JLabel("New label");
+		sl_panel_1.putConstraint(SpringLayout.NORTH, lblNewLabel_3, 0, SpringLayout.NORTH, textField_2);
+		sl_panel_1.putConstraint(SpringLayout.EAST, lblNewLabel_3, 0, SpringLayout.EAST, lblNewLabel_1);
+		panel_1.add(lblNewLabel_3);
+		
+		lblNewLabel_4 = new JLabel("New label");
+		sl_panel_1.putConstraint(SpringLayout.NORTH, lblNewLabel_4, 0, SpringLayout.NORTH, textField_3);
+		sl_panel_1.putConstraint(SpringLayout.WEST, lblNewLabel_4, 0, SpringLayout.WEST, lblNewLabel_1);
+		panel_1.add(lblNewLabel_4);
 		
 		mPersonajes = new DefaultListModel<>();
 		
